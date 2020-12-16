babec9aed60afe3edf34c34108dcbd7d1e44e531
Saeed Rahman <saeedrahman@cdnis.edu.hk>
2020-12-16 17:39:47 +0800

    Configured UITableView
    - added a Task data model (tasks array currently holds test values, but will need to be changed to be empty array of Strings)
    - conform TaskViewController to UITableViewDelegate and UITableViewDataSource protocols
    - configured tableview delegate and data source, pointing to Task data model

---
+++ Listify/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  Listify
+//
+//  Created by Saeed Rahman on 16/12/2020.
+//
+
+import Foundation
+
+class Task {
+    
+    static var tasks = ["Task 1", "Task 2", "Task 3"]
+}
+
--- Listify/TasksViewController.swift
+++ Listify/TasksViewController.swift
@@ -7,8 +7,20 @@
 
 import UIKit
 
-class TasksViewController: UIViewController {
-
+class TasksViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Task.tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = Task.tasks[indexPath.row]
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
