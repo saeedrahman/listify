32aa6ababa7020b9832e4e96b170d4dcc0657508
Saeed Rahman <saeedrahman@cdnis.edu.hk>
2020-12-16 13:07:08 +0800

    Initial Project Configuration
    - created and configured new view to be used for adding tasks
    - added UIKit elements such as table view, textfields and buttons
    - renamed the existing ViewController class and created a new class (AddTaskViewController)
    - configured large titles including aesthetic changes (colour, font size)
    - embedded views as part of a UINavigationController stack

---
+++ Listify/AddTaskViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddTaskViewController.swift
+//  Listify
+//
+//  Created by Saeed Rahman on 16/12/2020.
+//
+
+import UIKit
+
+class AddTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
